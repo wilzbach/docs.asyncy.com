1b31b74a9cc240fc71b29ec5c2bab94c6e5ce7ea
Jean Barriere <jean@barriere.io>
2019-04-12 17:17:28 +0200

    feat(theme): add layout theme

--- .vuepress/theme/Navbar.vue
+++ .vuepress/theme/Navbar.vue
@@ -1,15 +1,28 @@
 <template>
   <header class="navbar">
-    <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')"/>
-    <router-link to="/" class="home-link">
+    <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')" />
+    <router-link
+      to="/"
+      class="home-link"
+    >
       <!-- <a-logo /> -->
-      <a-logo icon variant="light" />
-      <a-badge state="primary" class="text--light home-badge" lower>Beta</a-badge>
+      <a-logo
+        icon
+        variant="light"
+      />
+      <a-badge
+        state="primary"
+        class="text--light home-badge"
+        lower
+      >Beta</a-badge>
     </router-link>
     <div class="links">
-      <AlgoliaSearchBox v-if="isAlgoliaSearch" :options="algolia"/>
-      <SearchBox v-else-if="$site.themeConfig.search !== false"/>
-      <NavLinks class="can-hide"/>
+      <AlgoliaSearchBox
+        v-if="isAlgoliaSearch"
+        :options="algolia"
+      />
+      <SearchBox v-else-if="$site.themeConfig.search !== false" />
+      <NavLinks class="can-hide" />
     </div>
   </header>
 </template>
@@ -32,59 +45,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-@import "@asyncy/vue/dist/sass/utils/_breakpoint.scss";
-
-.home-link {
-  // vertical-align: middle;
-  display: inline-flex !important;
-  flex: 0 0 auto;
-  flex-direction: row;
-  justify-content: left;
-  align-items: center;
-  height: 100%;
-  padding: .75rem 2rem;
-  svg {
-    height: 30px;
-    margin-right: .5rem;
-  }
-  .home-badge {
-    @include breakpoint(max s) {
-      display: none !important;
-    }
-  }
-}
-</style>
-<style lang="stylus">
-@import './styles/config.styl'
-
-.navbar
-  // padding 1.3rem 30px
-  // line-height $navbarHeight - 2.6rem
-  position relative
-  a, span, img
-    display inline-block
-  .logo
-    height $navbarHeight - 2.7rem
-    min-width $navbarHeight - 2.6rem
-    margin-right 0.8rem
-    vertical-align top
-  .links
-    font-size 0.9rem
-    position absolute
-    padding-right 2rem
-    padding-top 0
-    top 0
-    right 0
-    height 100%
-    display flex
-    align-items center
-    box-sizing border-box
-
-@media (max-width: $MQMobile)
-  .navbar
-    padding-left 4rem
-    .can-hide
-      display none
-</style>
--- .vuepress/theme/Sidebar.vue
+++ .vuepress/theme/Sidebar.vue
@@ -1,20 +1,60 @@
 <template>
-  <div class="sidebar" v-scroll="handleScroll">
-    <div :class="{ 'sidebar-internal': true, 'shorten': shortenSidebar }">
-    <NavLinks/>
-    <slot name="top"/>
-    <ul class="sidebar-links" v-if="items.length">
-      <li v-for="(item, i) in items">
-        <SidebarGroup v-if="item.type === 'group'"
-          :item="item"
-          :first="i === 0"
-          :open="i === openGroupIndex"
-          :collapsable="item.collapsable"
-          @toggle="toggleGroup(i)"/>
-        <SidebarLink v-else :item="item"/>
-      </li>
-    </ul>
-    <slot name="bottom"/>
+  <div
+    class="sidebar"
+    v-scroll="handleScroll"
+  >
+    <div class="is-hidden-tablet">
+      <div class="sidebar-selector-inactive">
+        <a-select
+          :value="getTitle"
+          fold-icon
+          @open="openSidebarAndCurrentItem"
+        />
+      </div>
+      <div class="sidebar-selector-active">
+        <a-select
+          v-for="(item, i) of items"
+          ref="sidebarItems"
+          @close="closeSidebarOnAllSelectClosed"
+          :value="item.title"
+          :path="item.path"
+          :key="`mobile-sidebar-${_uid}-${i}`"
+        >
+          <li
+            v-for="(child, cid) in item.headers"
+            :key="`mobile-sidebar-${_uid}-${i}-${cid}`"
+          >
+            <router-link
+              @click.native="closeSidebar"
+              :to="`${item.path}#${child.slug}`"
+            >{{ child.title }}</router-link>
+          </li>
+        </a-select>
+      </div>
+    </div>
+    <div :class="{ 'is-hidden-mobile': true, 'sidebar-internal': true, 'shorten': shortenSidebar }">
+      <NavLinks />
+      <slot name="top" />
+      <ul
+        class="sidebar-links"
+        v-if="items.length"
+      >
+        <li v-for="(item, i) in items">
+          <SidebarGroup
+            v-if="item.type === 'group'"
+            :item="item"
+            :first="i === 0"
+            :open="i === openGroupIndex"
+            :collapsable="item.collapsable"
+            @toggle="toggleGroup(i)"
+          />
+          <SidebarLink
+            v-else
+            :item="item"
+          />
+        </li>
+      </ul>
+      <slot name="bottom" />
     </div>
   </div>
 </template>
@@ -28,6 +68,12 @@ import { isActive } from './util'
 export default {
   components: { SidebarGroup, SidebarLink, NavLinks },
   props: ['items'],
+  computed: {
+    getTitle: function () {
+      if (this.$route.path === '/') return 'Home'
+      return this.items.find(i => i.path === this.$route.path).title
+    }
+  },
   data () {
     return {
       openGroupIndex: 0,
@@ -43,6 +89,23 @@ export default {
     }
   },
   methods: {
+    closeSidebarOnAllSelectClosed: function () {
+      if (this.$refs.sidebarItems.findIndex(item => item.open === true) === -1) {
+        this.closeSidebar()
+      }
+    },
+    closeSidebar: function () {
+      this.$emit('toggle-sidebar', false)
+    },
+    openSidebarAndCurrentItem: function () {
+      const current = this.$refs.sidebarItems.find(i => i.$attrs.path === this.$route.path)
+      if (current) {
+        // current.open = false
+        current.invalidate = true
+        current.open = true
+      }
+      this.$emit('toggle-sidebar', true)
+    },
     refreshIndex () {
       const index = resolveOpenGroupIndex(
         this.$route,
@@ -80,51 +143,135 @@ function resolveOpenGroupIndex (route, items) {
 </script>
 
 <style lang="stylus">
-@import './styles/config.styl'
+@import './styles/config.styl';
 
-.sidebar.home-sidebar .sidebar-internal
-  position relative
+.sidebar.home-sidebar .sidebar-internal {
+  position: relative;
 
-  &.shorten
-    position relative
-    bottom auto
+  &.shorten {
+    position: relative;
+    bottom: auto;
+  }
+}
 
-.sidebar
-  display inline-block
-  height auto
-  .sidebar-internal.shorten
-    position absolute
-    bottom 10px
+.sidebar {
+  height: auto;
 
-  .sidebar-internal
-    position fixed
-    height "calc(100vh - %s)" % $navbarHeight
-    width $sidebarWidth
-    overflow auto
-  ul
-    padding 0
-    margin 0
-    list-style-type none
-  a
-    display inline-block
-  .nav-links
-    display none
-    border-bottom 1px solid $borderColor
-    padding 0.5rem 0 0.75rem 0
-    a
-      font-weight 600
-    .nav-item, .repo-link
-      display block
-      line-height 1.25rem
-      font-size 1.1em
-      padding 0.5rem 0 0.5rem 1.5rem
-  .sidebar-links
-    padding 1.5rem 0
+  .sidebar-selector-inactive {
+    display: block;
+  }
 
-@media (max-width: $MQMobile)
-  .sidebar
-    .nav-links
-      display block
-    .sidebar-links
-      padding 1rem 0
+  .sidebar-selector-active {
+    display: none;
+  }
+
+  .sidebar-internal.shorten {
+    // position: absolute;
+    bottom: 0;
+  }
+
+  .sidebar-internal {
+    // position: fixed;
+    // height: 'calc(100vh - %s)' % $navbarHeight;
+    height: 100vh;
+    // width: $sidebarWidth;
+    width: 100%;
+    overflow: auto;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+  }
+
+  a {
+    display: inline-block;
+  }
+
+  .nav-links {
+    display: none;
+    border-bottom: 1px solid $borderColor;
+    padding: 0.5rem 0 0.75rem 0;
+
+    a {
+      font-weight: 600;
+    }
+
+    .nav-item, .repo-link {
+      display: block;
+      line-height: 1.25rem;
+      font-size: 1.1em;
+      padding: 0.5rem 0 0.5rem 1.5rem;
+    }
+  }
+
+  .sidebar-links {
+    // padding: 1.5rem 0;
+  }
+
+  .sidebar-links {
+    padding-left: 2rem;
+    position: relative;
+
+    li {
+      font-size: nth($sizes, 7);
+      color: nth($grays, 2);
+
+      a {
+        color: nth($grays, 2);
+      }
+
+      font-weight: 600;
+      padding: 0.25rem;
+      margin-top: 0.75rem;
+
+      &:last-child {
+        padding-bottom: 0.75rem;
+      }
+
+      cursor: pointer;
+
+      .sidebar-sub-headers {
+        border-left: 2px solid #c6c7dc;
+        margin-left: 1rem;
+      }
+
+      &.sidebar-sub-header {
+        a {
+          color: #7b7b8f;
+
+          &.active {
+            color: #000;
+            font-weight: bold;
+          }
+        }
+      }
+
+      .tags {
+        margin-top: 0.5rem;
+      }
+
+      &:not(.sidebar-sub-header) > a.active {
+        position: relative;
+        color: $accentColor;
+        font-weight: bold;
+
+        &:before {
+          content: '';
+          display: block;
+          width: 0.25rem;
+          height: 2rem;
+          position: absolute;
+          top: -0.2rem;
+          left: 0;
+          margin-left: -1.375rem;
+          bottom: 0;
+          border-radius: 0.25rem;
+          background-color: $accentColor;
+        }
+      }
+    }
+  }
+}
 </style>
