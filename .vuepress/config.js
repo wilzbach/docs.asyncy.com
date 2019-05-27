8a210205a5f0c1672f0e59fbd081c75e6f170654
Steve Peak <steve@stevepeak.net>
2019-05-27 11:29:39 +0200

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -54,6 +54,7 @@ module.exports = {
           '/storyscript/functions/',
           '/storyscript/flow/',
           '/storyscript/advanced/',
+           '/storyscript/secrets/',
         ]
       },
       '/services/',
